252dafa7fb9986a7ccf49e6afbdb667b1f24c630
Dawid Ginter <d.ginter@infusio.pl>
2020-04-17 02:14:08 +0200

    Dev1 added

---
+++ src/edu/utp/Dev1.java
@@ -0,0 +1,14 @@
+package edu.utp;
+
+public class Dev1 {
+
+    private String author = "dawin002";
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
