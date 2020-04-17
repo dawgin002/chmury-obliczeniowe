b5d995ccffbca968b4f7b83fddb6fc04a2c09acd
Dawid Ginter <d.ginter@infusio.pl>
2020-04-17 02:51:44 +0200

    Authors added in main

--- src/edu/utp/Main.java
+++ src/edu/utp/Main.java
@@ -2,12 +2,15 @@ package edu.utp;
 
 public class Main {
 
-    private static final String PROJECT_NAME = "Projekt na chmury obliczeniowe";
+    private static Dev1 d1 = new Dev1();
+    private static Dev2 d2 = new Dev2();
 
     public static void main(String[] args) {
         System.out.println("Wersja JRE: "+System.getProperty("java.runtime.version"));
         System.out.println("Wersja JDK: "+System.getProperty("java.version"));
         System.out.println("Nazwa: "+ System.getProperty("java.runtime.name"));
         System.out.println("Vendor: "+ System.getProperty("java.vm.vendor"));
+
+        System.out.println("Autorzy: "+d1.getAuthor()+", "+d2.getAuthor());
     }
 }
