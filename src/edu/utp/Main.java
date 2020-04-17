67239d1d400d3ddf215cd1095018033326bd3393
Dawid Ginter <d.ginter@infusio.pl>
2020-04-17 02:02:07 +0200

    Project name added

--- src/edu/utp/Main.java
+++ src/edu/utp/Main.java
@@ -2,6 +2,8 @@ package edu.utp;
 
 public class Main {
 
+    private static final String PROJECT_NAME = "Projekt na chmury obliczeniowe";
+
     public static void main(String[] args) {
         System.out.println("Wersja JRE: "+System.getProperty("java.runtime.version"));
         System.out.println("Wersja JDK: "+System.getProperty("java.version"));
