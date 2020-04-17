7c92cc818071b1f1410f323e59fc1a8cc553188d
Dawid Ginter <dginter89@gmail.com>
2020-04-17 02:23:13 +0200

    New, Dev2 class added

---
+++ src/edu/utp/Dev2.java
@@ -0,0 +1,14 @@
+package edu.utp;
+
+public class Dev2 {
+
+    private String author = "dejff";
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
